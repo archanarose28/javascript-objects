8e47f7aac71be5d8cfdd48890cb99c5236c1d80e
Archana Rose Biju <75205425+archanarose28@users.noreply.github.com>
2021-09-04 00:06:17 +0530

    updated

--- test/testMapObject.js
+++ test/testMapObject.js
@@ -1,12 +1,15 @@
 const res=require("../mapObject.js").f1;
-const res2=require("../mapObject.js").f2;
-//The input object data
 const objectInp={one: 1, two: 2, three: 3,four:4};
 const objEntries = Object.entries(objectInp);
 //before mapping the values are:
 console.log("before mapping :",Object.fromEntries(objEntries));
-//object is passed to the funtion
-computedValue=res(objectInp,res2);
+let computedValue;
+computedValue=res(objectInp,callbackmap); //object is passed to the funtion
 //printing resultant object to console in new array
 console.log("after mapping  :",computedValue);
+//callback function
+function callbackmap(keyOf,valueOf) {
+    console.log(keyOf,valueOf);
+    return valueOf*3;   //triple each property
+ }
 
